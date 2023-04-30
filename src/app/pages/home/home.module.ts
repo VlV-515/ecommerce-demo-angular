00ca9a51f74bac06c9d0e747a8c35b636abfc0e5
VlV <92279938+VlV-515@users.noreply.github.com>
2023-04-29 22:49:54 -0600

    Style🎨 add index home layout

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { IndexHomeComponent } from './components/index-home/index-home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MaterialModule } from 'src/app/shared/material.module';
 
 @NgModule({
   declarations: [IndexHomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, MaterialModule],
 })
 export class HomeModule {}
