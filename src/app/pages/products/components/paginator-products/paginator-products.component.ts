256f36d1f22f645e7f6bd4fe02abec0e9178c4a0
VlV <92279938+VlV-515@users.noreply.github.com>
2023-04-30 17:35:46 -0600

    Feat⚙️ add paginator products

---
+++ src/app/pages/products/components/paginator-products/paginator-products.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-paginator-products',
+  templateUrl: './paginator-products.component.html',
+  styleUrls: ['./paginator-products.component.scss'],
+})
+export class PaginatorProductsComponent {
+  constructor() {}
+}
--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -6,12 +6,14 @@ import { IndexProductsComponent } from './components/index-products/index-produc
 import { MaterialModule } from 'src/app/shared/material.module';
 import { SearchProductsTabComponent } from './components/search-products-tab/search-products-tab.component';
 import { CardProductComponent } from './components/card-product/card-product.component';
+import { PaginatorProductsComponent } from './components/paginator-products/paginator-products.component';
 
 @NgModule({
   declarations: [
     IndexProductsComponent,
     SearchProductsTabComponent,
     CardProductComponent,
+    PaginatorProductsComponent,
   ],
   imports: [CommonModule, ProductsRoutingModule, MaterialModule],
 })
