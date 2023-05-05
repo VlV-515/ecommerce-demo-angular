a737d59772c94b7453e2c393321443737e5f7e03
VlV <92279938+VlV-515@users.noreply.github.com>
2023-05-04 23:36:06 -0600

    Fix🪲 path in import

--- src/app/pages/checkout/checkout.module.ts
+++ src/app/pages/checkout/checkout.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CheckoutRoutingModule } from './checkout-routing.module';
-import { IndexCheckoutComponent } from './index-checkout/index-checkout.component';
-
+import { IndexCheckoutComponent } from './components/index-checkout/index-checkout.component';
 
 @NgModule({
-  declarations: [
-    IndexCheckoutComponent
-  ],
-  imports: [
-    CommonModule,
-    CheckoutRoutingModule
-  ]
+  declarations: [IndexCheckoutComponent],
+  imports: [CommonModule, CheckoutRoutingModule],
 })
-export class CheckoutModule { }
+export class CheckoutModule {}
