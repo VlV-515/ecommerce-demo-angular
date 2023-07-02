87a808ba4296fa558592aab27c6ade8c01d57209
VlV <92279938+VlV-515@users.noreply.github.com>
2023-07-01 19:37:43 -0600

    Feat⚙️Add footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { MaterialModule } from './material.module';
 import { RouterModule } from '@angular/router';
 import { ModalCartComponent } from './components/modal-cart/modal-cart.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [NavbarComponent, ModalCartComponent],
+  declarations: [NavbarComponent, ModalCartComponent, FooterComponent],
   imports: [CommonModule, MaterialModule, RouterModule],
-  exports: [NavbarComponent, RouterModule],
+  exports: [NavbarComponent, RouterModule, FooterComponent],
   entryComponents: [ModalCartComponent],
 })
 export class SharedModule {}
